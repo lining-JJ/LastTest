0bd13d7af5a1a95419bf18fbbfe4cf32db936035
lining-JJ <1461265200@qq.com>
2022-02-23 10:14:05 +0800

    first commit

---
+++ src/demo.java
@@ -0,0 +1,16 @@
+/**
+ * @Description
+ * @Author Lining
+ * @Version V1.0.0
+ * @Since 1.0
+ * @Date 2022/2/23
+ */
+
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+}
+
+
